8ec15bac694fd12e2aa82eb9f5da9729ae37a603
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-09-08 23:47:51 +0530

    flask app added

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, render_template, jsonify
+import pandas as pd
+from kafka import KafkaProducer, KafkaConsumer
+import json
+from threading import Thread
+from werkzeug.utils import redirect
+from random import randint
+# from pyhive import hive
+
+import pickle
+
+from preprocessor import *
+
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, template_folder="templates")
+app.config.from_pyfile('application.cfg', silent=True)
+# hive_engine = hive.Connection(
+#     host = app.config["HIVE_HOST"], 
+#     port = app.config["HIVE_PORT"],
+#     username = app.config["HIVE_USERNAME"],
+#     password = app.config["HIVE_PASSWORD"],
+# )
+
+
+producer = KafkaProducer(
+    bootstrap_servers = app.config["KAFKA_SERVER"],
+    value_serializer = lambda v: json.dumps(v).encode('utf-8')
+)
+
+@app.route("/", methods=["GET"])
+def home():
+    if request.method == "GET":
+        return render_template(
+            "test_model.html",
+        )
+
+def df_send_kafka(df, test_id=None):
+    print('send thread started')
+    for data in json.loads(df.to_json(orient="records")):
+        if test_id:
+            data["test_id"] = test_id
+        producer.send(topic=app.config["TOPIC"], value=data)
+
+
+@app.route("/test_report", methods=["GET", "POST"])
+def test_model():
+    if request.method == "GET":
+        return render_template(
+            "test_model.html",
+        )
+    elif request.method == "POST":
+        test_set = pd.read_csv(request.files["file"])
+        test_id = str(randint(0,99999999999))+str(randint(0,99999999999))+str(randint(0,99999999999))
+        thread = Thread(target=df_send_kafka, args=(test_set,test_id))
+        thread.start()
+        return redirect("/test_report?test_id="+test_id)
+
+
+@app.route("/house_price_predict", methods=["GET", "POST"])
+def house_price_predict():
+    predict_sale = ""
+    requst_data = dict(request.form)
+    if request.method=="POST":
+        print("POST request", requst_data, )
+    columns = pickle.load(open("data/columns", "rb"))
+    form_fields = json.load(open(path+"/model_form.json"))
+    form_fields_new = []
+    for obj in form_fields:
+        if obj["field"] not in columns:
+            continue
+        if requst_data:
+            obj["value"] = requst_data.get(obj["field"], "")
+        if obj.get("type") == "str":
+            obj["options"] = globals()[f'lst_{obj["field"]}_cat']
+        form_fields_new.append(obj)
+    
+    print(len(form_fields_new))
+    if request.method == "POST":
+        producer.send(topic=app.config["TOPIC"], value=json.dumps(requst_data))
+    return render_template(
+        "houseprice.html",
+        form_fields=form_fields_new,
+        predict_sale=predict_sale
+    )
+
